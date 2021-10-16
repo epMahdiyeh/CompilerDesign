97a9ceda9c587f272b0592dfaeb4b3e448b30e18
sogol <sogolsadeghid@gmail.com>
2021-10-16 14:56:05 +0330

    start state=1 -3

--- scanner.py
+++ scanner.py
@@ -6,27 +6,52 @@ def cur_state(state:int,char:str):
     symbol = [";", ":", ",", "[", "]", "(", ")", "{", "}", "+", "-" ,"*", "<"]
     whitespace = [" ", "\n", "\t", "\r", "\f", "\v"]
     keywords = ["if", "else", "void", "int", "repeat", "break", "until", "return"]
-    out = [False, "next_state or error type", False]
-
+    out =  ["next_state or error type"]
+    error=False
+    end=False
     if (state == 0):
         # keyword an id
         if(re.match(char,'[a-zA-Z]')):
-            out[1]=1
+            out = 1
         #num
         elif(re.match(char,'[0-9]')):
-            out[1]=3
+            out = 3
         #=
         elif(re.match(char=="=")):
-            out[1]=4
+            out = 4
         #symbol exept for =
         elif(char in symbol):
-            out[1]=7
+            out = 7
         #comment
         elif(char=='/'):
-            out[1]=8
+            out=8
         #whitespace
-        elif(char in whitespace):
-            out[1]=14
+        elif ( char in whitespace ) :
+            out = 14
+        else:
+         error=True
+         out="Invalid input"
+    elif(state==1):
+        if(re.match(char,'[a-zA-Z0-9]')):
+            out=1
+        elif(char in whitespace+symbol+'='):
+            out=15
+            end = True
+        else:
+            error=True
+            out="invalid input"
+    elif(state==3):
+        if(re.match(char,'[0-9]')):
+            out=3
+        elif((char in whitespace+symbol+'=') or (re.match(char,'[a-zA-z]'))):
+            out=16
+            end=True
+        else:
+            out="invalid number"
+
+
+
+
 
 
 
