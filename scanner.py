e8021bc59cb08485fc638f90b7ef3c055da77780
sogol <sogolsadeghid@gmail.com>
2021-10-16 17:22:09 +0330

    comment and whitespace are remain

--- scanner.py
+++ scanner.py
@@ -3,7 +3,7 @@ import os
 #def getNextToken():
 
 def cur_state(state:int,char:str):
-    symbol = [";", ":", ",", "[", "]", "(", ")", "{", "}", "+", "-" ,"*", "<"]
+    symbol = [";", ":", ",", "[", "]", "(", ")", "{", "}", "+", "-" , "<"]
     whitespace = [" ", "\n", "\t", "\r", "\f", "\v"]
     keywords = ["if", "else", "void", "int", "repeat", "break", "until", "return"]
     out =  ["next_state or error type"]
@@ -33,21 +33,67 @@ def cur_state(state:int,char:str):
          out="Invalid input"
     elif(state==1):
         if(re.match(char,'[a-zA-Z0-9]')):
-            out=1
-        elif(char in whitespace+symbol+'='):
-            out=15
+            out = 1
+        elif(char in whitespace+symbol + ["=", "*", "/"]):
+            out =15
             end = True
         else:
             error=True
             out="invalid input"
+    elif(state==2):
+        if(char=='/'):
+            out="unmatched comment"
     elif(state==3):
         if(re.match(char,'[0-9]')):
             out=3
-        elif((char in whitespace+symbol+'=') or (re.match(char,'[a-zA-z]'))):
+        elif((char in whitespace+symbol+["=", "*", "/"]) or (re.match(char,'[a-zA-z]'))):
             out=16
             end=True
         else:
             out="invalid number"
+            error=True
+    elif(state==4):
+        if(char=='='):
+            out=5
+        elif(char in whitespace+symbol+[ "*", "/"] or re.match(char,'[a-zA-Z0-9]')):
+            out=6
+            end=True
+        else:
+            error = True
+            out = "invalid input"
+    elif(state==5):
+        if(char in whitespace+symbol+["=", "*", "/"] or re.match(char,'[a-zA-Z0-9]')):
+            out=17
+        else:
+            error=True
+            out="invalid input"
+    elif(state==7):
+        if(char in whitespace+["=", "*", "/"] or re.match(char,'[a-zA-Z0-9]') ):
+            out=18
+        else:
+            error=True
+            out="invalid input"
+    elif(state==8):
+        if( char=='*'):
+            out=9
+        elif(char=='/'):
+            out=12
+    elif(state==9) :
+        if( not char=='*'):
+            out=9
+        elif(char=='*'):
+            out=10
+
+    #elif(state==10):
+        #if()
+
+    #elif(state==11):
+    #elif(state==12) :
+    #elif(state==13):
+
+
+   # elif(state==14):
+
 
 
 
