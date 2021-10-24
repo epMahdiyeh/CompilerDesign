abb0ed7999aa55309806e5638184d905dd141e32
epMahdiyeh <mahdiyeh.ep@gmail.com>
2021-10-24 15:09:07 +0330

    idk

--- scanner.py
+++ scanner.py
@@ -13,6 +13,58 @@ def lookahead_state(state:int,char:str):
     position =  "next_state or error type"
     error=False
     end=False
+
+    INPUT = open(os.path.realpath("./input/input.txt"), "rb")
+    errors = open(os.path.realpath("./ResultFiles/lexical_errors.txt"), "w")
+    symbols = open(os.path.realpath("./ResultFiles/symbol_table.txt"), "w")
+    tokens = open(os.path.realpath("./ResultFiles/tokens.txt"), "w")
+
+    add_symbols(symbols, KEYWORDS, 1)
+
+    line = 1
+    exist_error = False
+    change_line = False
+    first_line = True
+
+    while (True):
+        token = get_next_token(INPUT, KEYWORDS)
+
+        if (token[3] and token[2] == -1):
+            break
+
+        if (not (token[0] == "Invalid input" or token[0] == "Unclosed comment" or token[0] == "Invalid number" or token[
+            0] == "Unmatched comment" or token[0] == "COMMENT" or token[0] == "WHITESPACE")):
+            if (first_line or change_line):
+                if (not line == 1 and not first_line):
+                    tokens.write("\n")
+                tokens.write(str(line) + ".\t")
+                change_line = False
+                first_line = False
+            tokens.write("(" + token[0] + ", " + token[1] + ")" + " ")
+        elif (not (token[0] == "COMMENT" or token[0] == "WHITESPACE")):
+            exist_error = True
+            # -----------------------------------------------------------------------------------------unclosed comment-----------------------------------------------------------------
+        # if (token[0] == "Unclosed comment"):
+        # token[1] = format_unclosed_comment(token[1])
+        # errors.write(str(line) + ".\t" + "(" + token[1] + ", " + token[0] + ")\n")
+
+        if (token[0] == "ID" and token[1] not in ID):
+            ID.append(token[1])
+
+        if (token[2] > 0):
+            line += token[2]
+            change_line = True
+
+    if (not exist_error):
+        errors.write("There is no lexical error.")
+
+    add_symbols(symbols, ID, 11)
+
+    INPUT.close()
+    tokens.close()
+    errors.close()
+    symbols.close()
+
     if (state == 0):
         # keyword an id
         if(re.match(char,'[a-zA-Z]')):
@@ -178,6 +230,8 @@ def get_next_token (INPUT, KEYWORDS):
     else:
         resultForToken=["","",-1,False]
         return resultForToken
+
+
 
 
 
